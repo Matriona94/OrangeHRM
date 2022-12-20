df8420283be26562da9e5744a3a1bbaf502c9c8d
Matriona94 <102307287+Matriona94@users.noreply.github.com>
2022-12-20 15:36:25 +0100

    Update test_login.py

--- tests/test_scripts/test_login.py
+++ tests/test_scripts/test_login.py
@@ -1,34 +1,29 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
+from page_objects.login_page import LoginPage
+import pytest
 
 
-def test_login_test():
-    driver = webdriver.Chrome()
+class Test001Login:
+    baseURL = "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
+    username = "Admin"
+    password = "admin123"
 
-    driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+    def test_home_page_title(self, setup):
+        self.driver = setup
+        self.driver.get(self.baseURL)
+        act_title = self.driver.title
+        self.driver.close()
+        if act_title == "OrangeHRM":
+            assert True
+        else:
+            assert False
 
-    title = driver.title
-    assert title == "OrangeHRM"
-
-    driver.implicitly_wait(0.5)
-
-    text_box = driver.find_element(by=By.NAME, value="username")
-    text_box.send_keys("Admin")
-    text_box = driver.find_element(by=By.NAME, value="password")
-    text_box.send_keys("admin123")
-    submit_button = driver.find_element(by=By.CSS_SELECTOR, value="button")
-    submit_button.click()
-
-    import time
-
-    driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index")
-
-    time.sleep(3)
-
-    url = driver.current_url
-
-    driver.find_element(by=By.CLASS_NAME, value="oxd-userdropdown").click()
-    driver.find_element(by=By.LINK_TEXT, value="Logout").click()
-
-    driver.quit()
+    def test_login_test(self, setup):
+        self.driver = setup
+        self.driver.get(self.baseURL)
+        self.lp = LoginPage(self.driver)
+        self.lp.set_username(self.username)
+        self.lp.set_password(self.password)
+        self.lp.click_login()
+        self.lp.click_dropdown()
+        self.lp.click_logout()
 
